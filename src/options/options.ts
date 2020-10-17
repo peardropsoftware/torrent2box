11390f8dc30ce8ad635806446cc1fa5883748dd1
Rob Muchall <rmuchall@protonmail.com>
2020-10-17 10:07:05 +0100

    Tiny fix

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,6 +1,6 @@
 import "./options.scss";
 import Vue from "vue";
-import App from "./app.vue";
+import App from "./App.vue";
 import {paramCase} from "param-case";
 import {focusDirective} from "./directives/focus-directive";
 
